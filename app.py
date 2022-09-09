18b2d7d51a067cf25e6bb44867acb9d5e3c50e35
irish m <muyright22@gmail.com>
2022-09-09 21:06:02 +0800

    up

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def clinic():
 	return render_template("clinic.html", clinic = clinic)
 
 
-@app.route("/addclinic", methods = ['GET', 'POST'])
+@app.route("/addclinic", methods = ['POST'])
 def addclinic():
 	if request.method == 'POST':
 		clinic_services_name = request.form['clinic_services_name']
