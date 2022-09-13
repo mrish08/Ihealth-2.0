afad7e50f35cf62ef5123c74fe24f4f4feead39f
irish m <muyright22@gmail.com>
2022-09-13 15:56:05 +0800

    up

--- template/edit-medicine-resident.php
+++ template/edit-medicine-resident.php
@@ -1,5 +1,93 @@
-<?php include 'sidebar-resident.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Schedule| iHealth</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
+    <meta content="Coderthemes" name="author">
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="../../assets/images/brgyms-icon-white.png">
+
+    <!-- third party css -->
+    <link href="../../assets/css/vendor/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css">
+    <!-- third party css end -->
+
+    <!-- App css -->
+    <link href="../../assets/css/icons.min.css" rel="stylesheet" type="text/css">
+    <link href="../../assets/css/app-i.min.css" rel="stylesheet" type="text/css" id="light-style">
+    <link href="../../assets/css/app-dark.min.css" rel="stylesheet" type="text/css" id="dark-style">
+
+</head>
+
+<body class="loading" data-layout-config='{"leftSideBarTheme":"dark","layoutBoxed":false, "leftSidebarCondensed":false, "leftSidebarScrollable":false,"darkMode":false, "showRightSidebarOnStart": true}'>
+    <!-- Begin page -->
+    <header>
+    <div class="wrapper">
+        <!-- ========== Left Sidebar Start ========== -->
+        <div class="leftside-menu">
+
+            <!-- LOGO -->
+            <a href="admin-index.php" class="logo text-center logo-light">
+                <span class="logo-lg">
+                    <img src="../../assets/images/ihealth-logo-with-text.png" alt="" height="40">
+                </span>
+            </a>
+            <div class="h-100" id="leftside-menu-container" data-simplebar="">
+
+                <ul class="side-nav">
+
+                    <li class="side-nav-title side-nav-item">Menu</li>
+
+                    <li class="side-nav-item">
+                        <a href="index-resident.php" class="side-nav-link">
+                            <i class="uil-chart-pie"></i>
+                            <span> Dashboard </span>
+                        </a>
+                    </li>
+
+
+                    <li class="side-nav-item">
+                        <a href="vaccination-resident.php" class="side-nav-link">
+                            <i class="uil-syringe"></i>
+                            <span> Vaccination Service</span>
+                        </a>
+                    </li>
+
+                    <li class="side-nav-item">
+                        <a href="clinic-resident.php" class="side-nav-link">
+                            <i class="uil-clipboard-alt"></i>
+                            <span> Clinic Service </span>
+                        </a>
+                    </li>
+
+                    <li class="side-nav-item">
+                        <a href="dental-resident.php" class="side-nav-link">
+                            <i class="mdi mdi-hospital"></i>
+                            <span> Dental Service </span>
+                        </a>
+                    </li>
+
+                    <li class="side-nav-item">
+                        <a href="medicine-resident.php" class="side-nav-link">
+                            <i class="uil-home-alt"></i>
+                            <span>Medicine Pick-up</span>
+                    </li>
+                </ul>
+
+         
+            <div class="clearfix"></div>
+
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
+            <!-- Left Sidebar End -->
+
+        </div>
+        <!-- END wrapper -->
+</header>
+   
  <div class="wrapper">
         <div class="content-page">
             <div class="content">
--- template/vaccination-staff.php
+++ template/vaccination-staff.php
@@ -1,5 +1,106 @@
-﻿<?php include 'sidebar-staff.php';
-?>
+﻿<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>BITBo | iHealth</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
+    <meta content="Coderthemes" name="author">
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="../../assets/images/brgyms-icon-white.png">
+
+    <!-- third party css -->
+    <link href="../../assets/css/vendor/jquery-jvectormap-1.2.2.css" rel="stylesheet" type="text/css">
+    <!-- third party css end -->
+
+    <!-- App css -->
+    <link href="../../assets/css/icons.min.css" rel="stylesheet" type="text/css">
+    <link href="../../assets/css/app-i.min.css" rel="stylesheet" type="text/css" id="light-style">
+    <link href="../../assets/css/app-dark.min.css" rel="stylesheet" type="text/css" id="dark-style">
+
+</head>
+
+<body class="loading"
+    data-layout-config='{"leftSideBarTheme":"dark","layoutBoxed":false, "leftSidebarCondensed":false, "leftSidebarScrollable":false,"darkMode":false, "showRightSidebarOnStart": true}'>
+    <!-- Begin page -->
+    <header>
+
+    <div class="wrapper">
+        <!-- ========== Left Sidebar Start ========== -->
+        <div class="leftside-menu">
+
+            <!-- LOGO -->
+            <a href="admin-index.php" class="logo text-center logo-light">
+                <span class="logo-lg">
+                    <img src="../../assets/images/ihealth-logo-with-text.png" alt="" height="40">
+                </span>
+            </a>
+
+            <div class="h-100" id="leftside-menu-container" data-simplebar="">
+
+                <!--- Sidemenu -->
+                <ul class="side-nav">
+
+                    <li class="side-nav-title side-nav-item">Menu</li>
+
+                    <li class="side-nav-item">
+                        <a href="index-staff.php" class="side-nav-link">
+                            <i class="uil-chart-pie"></i>
+                            <span> Dashboard </span>
+                        </a>
+                    </li>
+
+                    <li class="side-nav-item">
+                        <a href="schedule-staff.php" class="side-nav-link">
+                            <i class="uil-clipboard-alt"></i>
+                            <span> Schedule </span>
+                        </a>
+                    </li>
+
+                    <li class="side-nav-item">
+                        <a href="vaccination-staff.php" class="side-nav-link">
+                            <i class="uil-syringe"></i>
+                            <span> Vaccination Service</span>
+                        </a>
+                    </li>
+
+                    <li class="side-nav-item">
+                        <a href="clinic-staff.php" class="side-nav-link">
+
+                            <i class="uil-clipboard-alt"></i>
+
+
+                            <span> Clinic Service </span>
+                        </a>
+                    </li>
+
+                    <li class="side-nav-item">
+                        <a href="dental-staff.php" class="side-nav-link">
+
+                            
+                            <i class="mdi mdi-hospital"></i>
+                            <span> Dental Service </span>
+
+                        </a>
+                    </li>
+
+                    <li class="side-nav-item">
+
+                        <a href="medicine-staff.php" class="side-nav-link">
+                            <i class="uil-home-alt"></i>
+                            <span>Medicine Pick-up</span>
+
+                       
+                    </li>
+                </ul>
+
+            </div>
+            <!-- Sidebar -left -->
+
+        </div>
+</header>
+
  <div class="wrapper">
 
         <div class="content-page">
