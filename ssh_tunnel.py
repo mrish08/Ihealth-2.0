9117843024c6dca910092a731ecd40709daba05c
irish m <muyright22@gmail.com>
2022-09-16 17:18:39 +0800

    s

--- ssh_tunnel.py
+++ ssh_tunnel.py
@@ -7,7 +7,7 @@ sshtunnel.TUNNEL_TIMEOUT = 5.0
 with sshtunnel.SSHTunnelForwarder(
     ('ssh.pythonanywhere.com'),
     ssh_username='iHealth', ssh_password='I2226rish',
-    remote_bind_address=('your PythonAnywhere database hostname, eg. yourusername-1234.postgres.pythonanywhere-services.com', the port on the databases page)
+    remote_bind_address=('iHealth-2829.postgres.pythonanywhere-services.com', 12829)
 ) as tunnel:
     connection = psycopg2.connect(
         user='postgres', password='123',
