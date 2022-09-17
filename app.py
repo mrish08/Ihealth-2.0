a1321944a000468c0aa4a310b65569e648bcb1eb
irish m <muyright22@gmail.com>
2022-09-17 21:05:09 +0800

    s

--- app.py
+++ app.py
@@ -257,8 +257,19 @@ def medicineresident():
 	return render_template("medicineresident.html")
 
 if __name__ == '__main__':
+	sshtunnel.SSH_TIMEOUT = 5.0
+	sshtunnel.TUNNEL_TIMEOUT = 5.0
 
-	app.debug=True
-	app.run(host='127.0.0.1', port=5000)
-
-	
+with sshtunnel.SSHTunnelForwarder(
+    ('ssh.pythonanywhere.com'),
+    ssh_username='iHealth', ssh_password='I2226rish',
+    remote_bind_address=('iHealth-2829.postgres.pythonanywhere-services.com', 12829)
+) as tunnel:
+    conn = psycopg2.connect(
+        user='postgres', password='123',
+        host='127.0.0.1', port=tunnel.local_bind_port,
+        database='iHealth_database',
+    )
+    conn.close()
+app.debug=True
+app.run(debug=True)
